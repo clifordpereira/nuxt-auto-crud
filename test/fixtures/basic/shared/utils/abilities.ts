163ebe7ea3ba2d7c4fe31ac0ab0a5ab286e466d6
Cliford Pereira <cliford.pereira@gmail.com>
2025-12-11 12:42:52 +0530

    chore: Remove redundant `no-unused-vars` eslint disable from ability logic.

--- test/fixtures/basic/shared/utils/abilities.ts
+++ test/fixtures/basic/shared/utils/abilities.ts
@@ -1,6 +1,6 @@
 import { defineAbility } from 'nuxt-authorization/utils'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const abilityLogic = (_user: any, _model: string, _action: string) => {
   return true
 }
