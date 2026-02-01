15895815ffbaad9526ed61a51465a210952ec2af
Cliford Pereira <cliford.pereira@gmail.com>
2026-02-01 17:28:24 +0530

    refactor: remove `__NAC_DISABLED__` and `hub:db` stubs.

--- src/runtime/server/stubs/hub-db.ts
+++
@@ -1,9 +0,0 @@
-// server/stubs/hub-db.ts
-export const db = {
-  select: () => {
-    throw new Error('Nuxt Hub (hub:db) not initialized. Ensure @nuxthub/core is installed and linked.')
-  },
-  insert: () => { throw new Error('hub:db stub: insert not available') },
-  update: () => { throw new Error('hub:db stub: update not available') },
-  delete: () => { throw new Error('hub:db stub: delete not available') }
-}
