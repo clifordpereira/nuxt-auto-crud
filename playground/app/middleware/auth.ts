6c43a7942a3632e2d03045e03d45af2437d2423a
Cliford Pereira <cliford.pereira@gmail.com>
2026-02-05 18:18:42 +0530

    chore: make `_meta` endpoint publicly accessible

--- playground/app/middleware/auth.ts
+++ playground/app/middleware/auth.ts
@@ -1,10 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
 
-  if (to.path.startsWith('/api/_nac/') && !loggedIn.value) {
-    return navigateTo('/')
-  }
-
   if (!loggedIn.value) {
     return navigateTo('/')
   }
