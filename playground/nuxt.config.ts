040f491b10f39166352e2f834dda32f9e68067fd
Cliford Pereira <cliford.pereira@gmail.com>
2026-02-13 13:22:31 +0530

    chore: prepare playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '../src/module'
-  ],
+  modules: ['../src/module', '@nuxthub/core'],
+  hub: { db: 'sqlite', kv: true }
 })
